04b1b178368a095411fac91d9d4cdea4c96ce7b5
Charlesnaldo <charles.naldo@gmail.com>
2026-01-31 00:04:21 -0300

    Fix: Refactor checkout page to properly use dynamic import with ssr: false

---
+++ src/app/checkout/CheckoutWrapper.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+
+const DynamicCheckoutClient = dynamic(() => import('./CheckoutClient'), {
+  ssr: false,
+  loading: () => <div>Loading checkout...</div>,
+});
+
+export default function CheckoutWrapper() {
+  return (
+    <Suspense fallback={<div>Loading checkout...</div>}>
+      <DynamicCheckoutClient />
+    </Suspense>
+  );
+}
--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,15 +1,5 @@
-import dynamic from 'next/dynamic';
-import { Suspense } from 'react';
-
-const DynamicCheckoutClient = dynamic(() => import('./CheckoutClient'), {
-  ssr: false,
-  loading: () => <div>Loading checkout...</div>,
-});
+import CheckoutWrapper from './CheckoutWrapper';
 
 export default function CheckoutPage() {
-  return (
-    <Suspense fallback={<div>Loading checkout...</div>}>
-      <DynamicCheckoutClient />
-    </Suspense>
-  );
+  return <CheckoutWrapper />;
 }
