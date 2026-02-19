c3683cf60ff48ce51ea56e1009d3d4e04d4ab59f
Charlesnaldo <charles.naldo@gmail.com>
2026-02-18 21:01:26 -0300

    Allow Supabase remote images in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "**.supabase.in",
+      },
+      {
+        protocol: "https",
+        hostname: "**.supabase.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
